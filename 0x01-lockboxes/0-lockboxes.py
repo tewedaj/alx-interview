3bf61a3d1b59c9acffdd5a48b22f94e96e5ed863
tewedaj <tewedaj13@gmail.com>
2023-01-31 14:06:24 +0300

    * long text break down

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -18,9 +18,9 @@ def canUnlockAll(boxes):
             unlockedList[x] = "always_unlockedList"
         for val in box:
             if val < len(boxes) and val != x:
-                """since each value is a key we use it to add value to our unlocked list
-                   we can add the size of the unlocked list and douplicates will overide on them self
-                """
+                """since each value is a key we use it to add value to our 
+                   unlocked list we can add the size of the unlocked list 
+                   and douplicates will overide on them self"""
                 unlockedList[val] = val
         if len(unlockedList) == len(boxes):
             return True
