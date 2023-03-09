1e831bbd237b148429865f6d41c7db646a98c281
tewedaj <tewedaj13@gmail.com>
2023-03-09 22:28:45 +0300

    updated formating

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,15 +3,15 @@
 const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+    if (err) throw err;
+    const actors = JSON.parse(body).characters;
+    exactOrder(actors, 0);
 });
 const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
+    if (x === actors.length) return;
+    request(actors[x], function (err, res, body) {
+        if (err) throw err;
+        console.log(JSON.parse(body).name);
+        exactOrder(actors, x + 1);
+    });
 };
