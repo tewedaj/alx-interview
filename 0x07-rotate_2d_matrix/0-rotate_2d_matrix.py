11bfd74396d8691dcfdd61babe4b75ca1d5f71fe
tewedaj <tewedaj13@gmail.com>
2023-03-16 15:50:27 +0300

    Formating updated

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,9 +11,9 @@ def rotate_2d_matrix(matrix):
     """
     n = len(matrix)
     for i in range(int(n / 2)):
-        y = (n - i - 1)
+        y = n - i - 1
         for j in range(i, y):
-            x = (n - 1 - j)
+            x = n - 1 - j
             tmp = matrix[i][j]
             matrix[i][j] = matrix[x][i]
             matrix[x][i] = matrix[y][x]
